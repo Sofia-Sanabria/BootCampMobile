e993244de52ce50a57dc552b61aa0c25978e8973
Sofia Montserrat Sanabria Estigarribia <smse2003@gmail.com>
2025-05-19 17:16:11 -0300

    Ejercicio Vacas y Toros

--- TareaBootCampSofi.c
+++ TareaBootCampSofi.c
@@ -1,3 +1,13 @@
+//
+// GitHub
+// Ejercicio realizado por Sofia Sanabria
+//
+// Ejercicio Vacas y Toros
+//
+// Created by Bootcamp on 2025-05-12.
+//
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
