4b76ce8af5d89506396d3b2451801a4f7b9399d7
Sofia Montserrat Sanabria Estigarribia <smse2003@gmail.com>
2025-05-19 14:59:40 -0300

    Update Ejercicio1_SofiaSanabria.swift

--- Ejercicio1_SofiaSanabria.swift
+++ Ejercicio1_SofiaSanabria.swift
@@ -1,5 +1,5 @@
 /** Ejercicio 1: Declara dos variables numéricas (con el valor que desees), muestra por consola la suma, resta, multiplicación, división y módulo (resto de la división). */
-
+//comentario
 var numero1 = 16
 var numero2 = 4
 
